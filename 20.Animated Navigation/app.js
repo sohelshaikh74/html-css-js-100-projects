eed6fff160ef09c4dc523f1235ffb00463912c57
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-01 23:26:16 +0530

    completed

---
+++ 20.Animated Navigation/app.js
@@ -0,0 +1,12 @@
+// toggle
+// navigagion
+
+
+const toggle = document.getElementById('toggle')
+
+const nav = document.getElementById('nav')
+
+
+toggle.addEventListener('click', ()=>{
+    nav.classList.toggle('active')
+})

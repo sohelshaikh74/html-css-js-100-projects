5d58447457f35fa43c9b45b1660cedc399f8f291
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-03 21:26:47 +0530

    the practice file updated

--- practice/app.js
+++ practice/app.js
@@ -1,12 +1,8 @@
-// how to find fibonacci sequence in javascript
-let num = 5
-let temp = num;
-let a = 0 ;
-let b = 1;  
-while(num>0){
-   temp = temp%10
-   a = b;
-   b = temp;
-   temp = parseInt(temp /10)
-   console.log(temp);
-}
+let indicator = document.querySelector('.scroll-indicator')
+let scrollHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight
+
+window.addEventListener('scroll',()=>{
+    let scrollTop = document.documentElement.scrollTop
+    let scrolled = (scrollTop/scrollHeight)*100
+    indicator.style.width=`${scrolled}%`
+})

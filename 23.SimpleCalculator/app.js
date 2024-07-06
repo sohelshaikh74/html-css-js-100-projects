731abab6276e92954242303fedf8d8f8c15a5342
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-07 01:54:36 +0530

    .

---
+++ 23.SimpleCalculator/app.js
@@ -0,0 +1,6 @@
+document.getElementById('btn').addEventListener('click',()=>{
+    let num1= document.querySelector('.num1').value
+    let num1= document.querySelector('.num1').value
+    let num1= document.querySelector('.num1').value
+    let num1= document.querySelector('.num1').value
+})

8b07dcf9f77a795296c1d99fe7a4497e31b1948b
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-08 21:08:04 +0530

    all update

--- 23.SimpleCalculator/app.js
+++ 23.SimpleCalculator/app.js
@@ -1,22 +1,20 @@
-
-// let btn = document.getElementById('btn')
-// let num1 = document.querySelector('.num1').value
-// let num2 = document.querySelector('.num2').value
-// let result = document.querySelector('.result')
-// let operator = document.getElementById('selectOp').value
-// btn.addEventListener('click',function(){
-//     switch(operator){
-//         case "plus":
-//             result.innerHTML=Number(num1)+Number(num2);
-//         break;
-//         case "min":
-//             result.innerHTML=Number(num1)-Number(num2);
-//         break;
-//         case "dev":
-//             result.innerHTML=Number(num1)/Number(num2);
-//         break
-//         case "mul":
-//             result.innerHTML=Number(num1)*Number(num2);
-//         break;
-//     }
-// })
+document.getElementById("btn").addEventListener("click", function () {
+    let num1 = document.querySelector(".num1").value;
+    let num2 = document.querySelector(".num2").value;
+    let result = document.querySelector(".result");
+    let oprator = document.getElementById("selectOp").value;
+  
+    switch (oprator) {
+      case "plus":
+        result.innerHTML = Number(num1) + Number(num2);
+        break;
+      case "min":
+        result.innerHTML = Number(num1) - Number(num2);
+        break;
+      case "dev":
+        result.innerHTML = Number(num1) / Number(num2);
+        break;
+      case "multi":
+        result.innerHTML = Number(num1) * Number(num2);
+    }
+  });

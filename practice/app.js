17d9b62c673b83128afb371c3cc25fae88333cf8
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-12 21:58:37 +0530

    practice file updated

--- practice/app.js
+++
@@ -1,8 +0,0 @@
-// input
-let input = document.querySelector('input')
-// eye
-let eyeIcon = document.querySelector('#eye-icon')
-
-eyeIcon.addEventListener('click',()=>{
-  input.type==="password"?input.type="text":input.type="password"
-})

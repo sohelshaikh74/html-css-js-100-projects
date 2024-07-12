2d1aa8a6cf5723ca1551285cb7ebad5eb18d5bf6
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-12 23:56:19 +0530

    add

---
+++ 29.Social Media Selector/app.js
@@ -0,0 +1,28 @@
+// selectField
+let selectField = document.getElementById('selectField')
+
+// selectText
+let selectText = document.getElementById('selectText')
+
+// list
+let list = document.getElementById('list')
+
+// arrowIcon
+let arrowIcon = document.getElementById('arrowIcon')
+
+// options
+
+let options = document.getElementsByClassName('options')
+
+
+// console.log((selectField));
+// console.log((selectText));
+// console.log((list));
+console.log(arrowIcon);
+// console.log(options);
+
+
+selectField.addEventListener('click',()=>{
+    list.classList.toggle('hide')
+    arrowIcon.classList.toggle('rotate')
+})

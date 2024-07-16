40f910ecc0f6ca542de8df24c01ddb1962b69298
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-16 22:24:08 +0530

    .

--- practice/app.js
+++ practice/app.js
@@ -1,9 +1,13 @@
 let input = document.querySelector('input')
-
-let eyeIcon=document.querySelector('#eye-icon')
-
+let eyeIcon = document.querySelector('#eye-icon')
+console.log(input);
+console.log(eyeIcon);
 eyeIcon.addEventListener('click',()=>{
-  input.type ==="password"?input.type="text":input.type="password"
+  // console.log('hello');
+  if(input.type==='password'){
+    input.type="text"
+  }
+  else{
+    input.type="password"
+  }
 })
-
-

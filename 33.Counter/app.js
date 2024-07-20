7e3653fd14003e7a8547f9d7c11348c7ad6c34a8
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-20 17:05:45 +0530

    All element are selected

---
+++ 33.Counter/app.js
@@ -0,0 +1,13 @@
+// count ele
+let countEle = document.getElementById('count')
+// save ele
+let saveEle = document.getElementById('save')
+// increment
+let incrementBtn = document.querySelector('.increments-btn')
+// save-btn
+let saveBtn=document.querySelector('.save-btn')
+
+// console.log(countEle);
+// console.log(saveEle);
+// console.log(incrementBtn);
+// console.log(saveBtn);

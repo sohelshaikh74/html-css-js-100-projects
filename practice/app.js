a0ba965a6cab199b359c3a2319a6bfb99c36ef12
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-21 02:15:02 +0530

    Practice file update

--- practice/app.js
+++ practice/app.js
@@ -1,9 +1,1 @@
-let cursor = document.querySelector('.cursor')
-
-document.addEventListener('mousemove',(event)=>{
-   moveCursor(event.pageX,event.pageY)
-})
-const moveCursor=(pageX,pageY)=>{
-   cursor.style.left = pageX + 'px'
-   cursor.style.top = pageY + 'px'
-}
+let btn = document.getElementById('btn')

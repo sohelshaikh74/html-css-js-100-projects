342927664492ec55dea4073ef4ef72b1b114034b
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-20 17:13:41 +0530

    Done

--- 33.Counter/app.js
+++ 33.Counter/app.js
@@ -11,3 +11,15 @@ let saveBtn=document.querySelector('.save-btn')
 // console.log(saveEle);
 // console.log(incrementBtn);
 // console.log(saveBtn);
+
+let count = 0
+incrementBtn.addEventListener('click',()=>{
+    count +=1
+    countEle.textContent = count
+})
+saveBtn.addEventListener('click',()=>{
+    let countStr = count + ", "
+    saveEle.textContent += countStr 
+    count = 0
+    countEle.textContent = count;
+})

50c672261977f5ea3cbd6a9e4d1f5840402f1145
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-20 17:16:51 +0530

    pratice

--- 33.Counter/app.js
+++ 33.Counter/app.js
@@ -1,25 +1,42 @@
-// count ele
+// // count ele
+// let countEle = document.getElementById('count')
+// // save ele
+// let saveEle = document.getElementById('save')
+// // increment
+// let incrementBtn = document.querySelector('.increments-btn')
+// // save-btn
+// let saveBtn=document.querySelector('.save-btn')
+
+// // console.log(countEle);
+// // console.log(saveEle);
+// // console.log(incrementBtn);
+// // console.log(saveBtn);
+
+// let count = 0
+// incrementBtn.addEventListener('click',()=>{
+//     count +=1
+//     countEle.textContent = count
+// })
+// saveBtn.addEventListener('click',()=>{
+//     let countStr = count + ", "
+//     saveEle.textContent += countStr 
+//     count = 0
+//     countEle.textContent = count;
+// })
+
+
+
 let countEle = document.getElementById('count')
-// save ele
-let saveEle = document.getElementById('save')
-// increment
+
+let saveBtn = document.querySelector('.save-btn')
+
 let incrementBtn = document.querySelector('.increments-btn')
-// save-btn
-let saveBtn=document.querySelector('.save-btn')
 
-// console.log(countEle);
-// console.log(saveEle);
-// console.log(incrementBtn);
-// console.log(saveBtn);
+let saveEle = document.getElementById('save')
 
-let count = 0
-incrementBtn.addEventListener('click',()=>{
-    count +=1
-    countEle.textContent = count
-})
-saveBtn.addEventListener('click',()=>{
-    let countStr = count + ", "
-    saveEle.textContent += countStr 
-    count = 0
-    countEle.textContent = count;
-})
+console.log(countEle);
+console.log(saveBtn);
+console.log(saveEle);
+console.log(incrementBtn);
+
+let count = 0;

410c5d777f273e9dc76a4926eb63de8ff8ac5c14
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-21 02:24:22 +0530

    updated

--- practice/app.js
+++ practice/app.js
@@ -1,1 +1,11 @@
-let btn = document.getElementById('btn')
+document.addEventListener('DOMContentLoaded', function() {
+   const changeColorBtn = document.getElementById('changeColorBtn');
+ 
+   changeColorBtn.addEventListener('click', function() {
+     // Generate a random color
+     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+ 
+     // Change the background color of the entire document
+     document.body.style.backgroundColor = randomColor;
+   });
+ });

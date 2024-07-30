9d3b7200f7402d70c0a70bb921543560352c2a72
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-07-30 22:14:01 +0530

    All Updated

--- 34.Text Formatter/app.js
+++ 34.Text Formatter/app.js
@@ -1,12 +1,35 @@
-let output = document.getElementById('output')
+// inputfield
+let inputField = document.getElementById('input-field')
+// outpoutfield
+let outputField = document.getElementById('output-field')
+// button
+let buttons = document.querySelectorAll('button')
 
-let input = document.getElementById('text-input')
-let capital = document.getElementById('capital')
-let small = document.getElementById('small')
+// console.log(inputFie 
 
-// (input.value);
-let array = []
-capital.addEventListener('click',()=>{
-        array = input.value
-        console.log(array);
-})
+inputField.addEventListener('keyup',()=> (outputField.innerHTML=inputField.value)
+);
+
+buttons.forEach(btn => {
+    btn.addEventListener('click',()=>{
+        if(btn.classList.contains('uppercase')){
+            outputField.innerHTML = outputField.innerHTML.toUpperCase()
+        }
+        else if(btn.classList.contains('lowercase')){
+            outputField.innerHTML = outputField.innerHTML.toLowerCase()
+        }
+        
+        else if(btn.classList.contains('capitalize')){
+            outputField.innerHTML = outputField.innerHTML.charAt(0).toUpperCase() + outputField.innerHTML.slice(1)
+        }
+        else if(btn.classList.contains('bold')){
+            outputField.style.fontWeight = "900";
+        }
+        else if(btn.classList.contains('italic')){
+            outputField.style.fontStyle ="italic"
+        }
+        else if(btn.classList.contains('underline')){
+            outputField.style.textDecoration ="underline"
+        }
+    })
+});

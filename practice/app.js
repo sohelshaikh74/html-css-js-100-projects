b2283ab78dd7a0042737ac48b65402269601fafa
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-08-06 14:04:21 +0530

    Practice file updated

--- practice/app.js
+++ practice/app.js
@@ -1,7 +1,1 @@
-document.getElementById('show-login').addEventListener('click',function(){
-   document.querySelector('.popup').classList.add('active')
-})
 
-document.querySelector('.popup .close-btn').addEventListener('click',function(){
-   document.querySelector('.popup').classList.remove('active')
-})

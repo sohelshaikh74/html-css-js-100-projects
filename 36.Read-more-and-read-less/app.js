0896c5c76d7d04e9bfb16e36caadfeb8bdf61aaa
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-09-05 14:22:04 +0530

    New project added

---
+++ 36.Read-more-and-read-less/app.js
@@ -0,0 +1,6 @@
+let containerTwo=document.querySelector('.content-container-2')
+let btn = document.querySelector('.btn')
+btn.addEventListener('click',()=>{
+    containerTwo.classList.toggle('toggle')
+    // alert(1)
+})

4556dc1659f28cb39cec088cb523e9a8f30694e3
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-09-28 19:09:13 +0530

    is palindrom

--- practice/app.js
+++ practice/app.js
@@ -9,3 +9,17 @@ console.log(reverseString(str));
 // find the largest number in the array
 let arr = [1,2,3]
 console.log(Math.max(...arr));
+
+// check palindrom or not
+let pal = "sohel"
+const isPalin = (pal)=>{
+  let revPal = pal.split("").reverse().join("")
+  if(revPal === pal){
+    return true
+  }
+  else {
+    return false
+  }
+}
+console.log(isPalin(pal));
+

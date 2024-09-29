06bb0379743fdce900e3d41a86a0b4bc0d77fc51
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-09-29 20:53:58 +0530

    All updated

--- practice/app.js
+++ practice/app.js
@@ -1,7 +1,19 @@
-let str = "sohel"
-const isPalndrom = (str)=>{
-  let result =  str.split("").reverse().join("")
-  return result === str;
-  
+// fibonaccie sequnece
+const fibonaccie =(n)=>{
+   if(n===0){
+    return 0
+   }
+   else if(n===1){
+    return 1
+   }
+   let a  = 0 ;
+   let b = 1;
+   let fib = 0 
+   for(let i=2;i<=n;i++){
+        fib = a+b;
+        a=b;
+        b = fib;
+   }
+   return fib
 }
-console.log(isPalndrom(str));
+console.log(fibonaccie(6));

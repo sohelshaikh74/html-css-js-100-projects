e517f24b1d363fb7f751f8f3b1d6ed483a44d47d
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-09-29 21:16:38 +0530

    .

--- practice/app.js
+++ practice/app.js
@@ -1,15 +1,28 @@
-// calcualte the fibonacci sequence
-const fibonacci = (n)=>{
-  if(n===0) return 0;
-  if(n===1) return 1;
-  let a = 0;
-  let b = 1;
-  let fib = 0
-  for(let i=2;i<=n;i++){
-    fib = a+b;
-    a = b;
-    b = fib
-  }
-  return fib
+// // count vowel in a string
+// const vowel = (str)=>{
+//     const vowels = "aeiouAEIOU"
+//     let count = 0;
+//     for(let char of str){
+//         if(vowels.includes(char)){
+//             count++
+//         }
+//     }
+//     return count
+  
+// }
+// console.log(vowel("sohel"));
+
+// find the messing number in the array
+const missingNumber = (arr)=>{
+   let maxValue = Math.max(...arr)
+   let minValue = Math.min(...arr)
+   let missingNumber = []
+   for(let i=minValue;i<=maxValue;i++){
+      if(arr.indexOf(i)<0){
+        missingNumber.push(i)
+      }
+   }
+   return missingNumber;
 }
-console.log(fibonacci(6));
+console.log(missingNumber([1,2,3,4,5]));
+

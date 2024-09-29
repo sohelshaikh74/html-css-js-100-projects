49852773eec338985f748977d13db56a74e65d60
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-09-29 20:59:43 +0530

    .

--- practice/app.js
+++ practice/app.js
@@ -1,19 +1,15 @@
-// fibonaccie sequnece
-const fibonaccie =(n)=>{
-   if(n===0){
-    return 0
-   }
-   else if(n===1){
-    return 1
-   }
-   let a  = 0 ;
-   let b = 1;
-   let fib = 0 
-   for(let i=2;i<=n;i++){
-        fib = a+b;
-        a=b;
-        b = fib;
-   }
-   return fib
+// calcualte the fibonacci sequence
+const fibonacci = (n)=>{
+  if(n===0) return 0;
+  if(n===1) return 1;
+  let a = 0;
+  let b = 1;
+  let fib = 0
+  for(let i=2;i<=n;i++){
+    fib = a+b;
+    a = b;
+    b = fib
+  }
+  return fib
 }
-console.log(fibonaccie(6));
+console.log(fibonacci(6));

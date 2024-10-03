7fa21ed0bf79c39fc41c0d54c5b1758aa534ef13
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-10-03 20:08:06 +0530

    Fizzbuzz problem solved

--- practice/app.js
+++ practice/app.js
@@ -1,10 +1,15 @@
-let str = "jhon"
-const revStr = (str)=>{
-  let reveresd = ""
-  for(let i=str.length-1;i>=0;i--){
-       reveresd += str[i]
-  }
-  return reveresd
+for(let i=1;i<=100;i++){
+  if(i%3===0 && i%5===0 ){
+     console.log('FizzBuzz');
+      }
+      else if(i%3===0){
+        console.log('Fizz')
+      }
+      else if(i%5===0){
+        console.log("Buzz")
+      }
+      else{
+        console.log(i);
+        
+      }
 }
-console.log(revStr(str));
-

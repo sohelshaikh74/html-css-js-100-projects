ff20e4fa51578f6261fe29d3762c6406684e086f
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-10-03 18:43:59 +0530

    New problems solved

--- practice/app.js
+++ practice/app.js
@@ -1,7 +1,26 @@
-// reverse a string
-let str = "Hello"
-const reverseStr = (str)=>{
-       let result =  str.split("").reverse().join("")
-       return result 
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//   } else {
+//       console.log(i);
+//   }
+// }
+
+for(let i=1;i<=100;i++){
+   if(i%3===0 && i %5 ===0){
+          console.log("FizzBuzz");
+   }
+   else if(i%3===0){
+    console.log('fizz');
+   }
+   else if(i%5===0){
+    console.log('buzz');
+   }
+   else{
+    console.log(i);
+   }
 }
-console.log(reverseStr(str));

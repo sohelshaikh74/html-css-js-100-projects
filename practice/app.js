ba48551798c0791b8ede5b7f87c7bd2039d4790d
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-10-04 18:24:53 +0530

    reveres string

--- practice/app.js
+++ practice/app.js
@@ -1,6 +1,10 @@
-let palin = "sohel"
-const isPal = (palin)=>{
-  let result = palin.split("").reverse().join("")
-  return result === palin
+// reverse a given string
+let str = "hello"
+const revStr = (str)=>{
+   let reversed = ""
+   for(let i=str.length-1;i>=0;i--){
+      reversed += str[i]
+   }
+   return reversed
 }
-console.log(isPal(palin));
+console.log(revStr(str));

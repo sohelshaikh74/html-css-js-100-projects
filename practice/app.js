c05b023c7d472eb379be69caa09a67e5a87e8622
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-10-05 12:44:35 +0530

    All updated

--- practice/app.js
+++ practice/app.js
@@ -1,36 +1,14 @@
-// // reverse a given string
-// let str = "hello"
-// const revStr = (str)=>{
-//    let reversed = ""
-//    for(let i=str.length-1;i>=0;i--){
-//       reversed += str[i]
-//    }
-//    return reversed
+// // what is rest operator in js
+// const addNum=(a,b,...c)=>{
+//   // console.log(a+b+c);
+//   let result = a+b //33,4
+//   let d = c
+//   // return result
+//   return d
 // }
-// console.log(revStr(str));
+// console.log(addNum(1,2,3,4));
 
-
-// let str2 = "world"
-// const revStr2 = (str2)=>{
-//   let result = str2.split("").reverse().join("")
-//   return result
-// }
-// console.log(revStr2(str2));
-
-// print fizzbuzz
-for(let i=1;i<=100;i++){
-   if(i%3===0 && i%5===0){
-    console.log('FizzBuzz');
-   }
-   else if(i%3===0){
-    console.log('Fizz');
-    
-   }
-   else if(i%5===0){
-   console.log('Buzz');
-   }
-   else{
-    console.log(i);
-    
-   }
-}
+// this is arrow function introduced in es6. it is simple and easy way to write a function
+let a = 10
+let b =[1,2]
+console.log(a-b);

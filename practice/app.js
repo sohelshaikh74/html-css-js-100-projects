46a4614a89128e54d0763cabb57645138640c412
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-10-18 19:51:00 +0530

    all updated

--- practice/app.js
+++ practice/app.js
@@ -1,1 +1,14 @@
-// 
+// write a js function that takes an array of numbers and returns a new array with only the even numbers
+const arr = [1,2,3,4,5]
+const evenArr =(arr)=>{
+  let evenArray = []
+  for(let i=0;i<arr.length;i++){
+     if(arr[i]%2==0){
+          evenArr.push(arr[i])
+     }
+  }
+  return evenArray
+}
+console.log(evenArr(arr));
+console.log('js');
+alert(1)

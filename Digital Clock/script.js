0c21981161f314b1e2506047d57b4e6a07ee950d
Sohel Shaikh <sohelshaikh30611@gmail.com>
2024-10-19 16:05:56 +0530

    all updated

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -4,6 +4,6 @@ let sec = document.getElementById('sec')
 setInterval(()=>{
     let currentTime = new Date()
      hrs.innerHTML=(currentTime.getHours()<10?"0":"")+currentTime.getHours();
-     min.innerHTML=(currentTime.getHours()<10?"0":"")+currentTime.getMinutes();
-     sec.innerHTML=(currentTime.getHours()<10?"0":"")+currentTime.getSeconds();
+     min.innerHTML=(currentTime.getMinutes()<10?"0":"")+currentTime.getMinutes();
+     sec.innerHTML=(currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
 },1000)
---
+++ Drag and Drop/script.js
@@ -0,0 +1,27 @@
+let lists = document.getElementsByClassName('list')
+console.log(lists);
+
+let rightBox = document.querySelector('.right')
+let leftBox = document.querySelector('.left')
+console.log(rightBox);
+console.log(leftBox);
+
+for(let list of lists){
+    list.addEventListener('dragstart',(e)=>{
+        let selected = e.target
+        rightBox.addEventListener('dragover',(e)=>{
+             e.preventDefault()
+        })
+        rightBox.addEventListener('drop',(e)=>{
+            rightBox.appendChild(selected)
+            selected= null
+        })
+        leftBox.addEventListener('dragover',(e)=>{
+             e.preventDefault()
+        })
+        leftBox.addEventListener('drop',(e)=>{
+            leftBox.appendChild(selected)
+            selected= null
+        })
+    })
+}
